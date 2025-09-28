d5849bdc9d0aff6295c99a6d7bb69ca9c83e4a58
peplinka <alisakylagina208@gmail.com>
2025-09-28 17:30:07 +0300

    feat: add webpack config and dev server

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+// webpack.config.js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      // Для JavaScript и JSX
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }],
+              ['@babel/preset-react', { runtime: 'automatic' }]
+            ],
+          },
+        },
+      },
+      // Для CSS ← ЭТО БЫЛО ПРОПУЩЕНО!
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: './public',
+    port: 8081,
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+  },
+  mode: 'development',
+};
